aa246d42a9f33e66676978eeab9fecf2368f3eb7
Clark Van Oyen <clark@countable.ca>
2016-03-28 15:21:29 -0700

    generate markdown TOC

--- bundle.js
+++ bundle.js
@@ -2,12 +2,31 @@
 
 var toc = require('markdown-toc');
 
+var select_all_on_click = function(textBox) {
+  textBox.onfocus = function() {
+    textBox.select();
+
+    // Work around Chrome's little problem
+    textBox.onmouseup = function() {
+        // Prevent further mouseup intervention
+        textBox.onmouseup = null;
+        return false;
+    };
+  };
+};
+
+select_all_on_click(document.getElementById('md'));
+select_all_on_click(document.getElementById('toc'));
+
 window.convert = function(e){
 
-  console.log('converting', document.getElementById('md').value, toc(document.getElementById('md').value).content);
   document.getElementById('toc').value
-  =toc(document.getElementById('md').value).content
-}
+  =toc(document.getElementById('md').value).content 
+  +"\n\n<small><i><a href='http://ecotrust-canada.github.io/markdown-toc/'>Table of contents generated with markdown-toc</a></i></small>\n";
+
+};
+
+convert();
 
 },{"markdown-toc":23}],2:[function(require,module,exports){
 
